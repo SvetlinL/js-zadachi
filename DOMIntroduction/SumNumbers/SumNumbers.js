e70ad1d6253452481efdd782ea7add9e6452289c
Svetlin <69974169+SvetlinL@users.noreply.github.com>
2021-09-23 20:20:59 +0300

    a

--- DOMIntroduction/SumNumbers/SumNumbers.js
+++ DOMIntroduction/SumNumbers/SumNumbers.js
@@ -1,3 +1,13 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    //read value of input fields
+    // sum values
+    //set result as value of output field
+    //!!!!!
+    // za tozi tip poleta ne se polzva textContent !!!!
+    //!!!!!
+    const a = Number(document.getElementById('num1').value);
+    const b = Number(document.getElementById('num2').value);
+    const sum = a+b;
+    document.getElementById('sum').value = sum;
+
 }
