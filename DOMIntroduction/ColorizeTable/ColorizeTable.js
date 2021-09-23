663c03fc5fe74b383538198e3a3bf940787e039d
Svetlin <69974169+SvetlinL@users.noreply.github.com>
2021-09-23 21:47:05 +0300

    a

--- DOMIntroduction/ColorizeTable/ColorizeTable.js
+++ DOMIntroduction/ColorizeTable/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+    const rows = document.querySelectorAll("table tr" );
+    //moje i "table tr: nth-child(even)" -> vrushta chetnite redove i obhovdame s i++
+
+    for (let i = 1; i < rows.length; i+=2) {
+        rows[i].style.backgroundColor = 'teal';
+    }
 }
