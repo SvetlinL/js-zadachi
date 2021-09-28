c787d5f9d92469ca81aa0fe1ee808b34736a7777
Svetlin <69974169+SvetlinL@users.noreply.github.com>
2021-09-28 21:39:29 +0300

    a

--- DOMEvents/Furniture/solution.js
+++ DOMEvents/Furniture/solution.js
@@ -1,4 +1,68 @@
 function solve() {
+    // # configuring eventListeners #
+    //select all buttons
+    // first button -> table generation
+    // second button -> buy furniture
 
-  //TODO...
+    const [input, output] = Array.from(document.querySelectorAll('textarea'));
+    const [generateBtn, buyBtn] = Array.from(document.querySelectorAll('button'));
+
+    generate.addEventListener('click', generate);
+    buyBtn.addEventListener('click', buy);
+
+    // # table generation #
+    function generate(e) {
+        // read input JSON and parse it
+        // for every array element create table row
+        const data =  JSON.parse(input.value);
+
+        for (let item of row){
+            const row = document.createElement('tr');
+
+            const imgCell = document.createElement('td');
+            const nameCell = document.createElement('td');
+            const priceCell = document.createElement('td');
+            const decFactorCell = document.createElement('td');
+            const checkCEll = document.createElement('td');
+
+            const img = document.createElement('img');
+            img.src = item.img;
+            imgCell.appendChild(img);
+        }
+    }
+
+
+    // # buy furniture #
+    function buy(e){
+        // select all checkboxes
+        // filter only checked checkboxes
+        // repeat for every selected checkbox
+        // -- select parent row
+        // -- read item information
+        // display output
+
+        const furniture = Array.from(document.querySelectorAll('input[type="checkbox"]:checked'))
+            .map(b=> b.parentElement.parentElement)
+            .map(r => ({
+                name: r.children[1].textContent,
+                price: Number(r.children[2].textContent),
+                decFactor: Number(r.children[3].textContent)
+            }));
+        let total = 0;
+        let decFactor = 0;
+        const names = [];
+
+        for (let item of furniture) {
+            total += item.price;
+            decFactor += item.decFactor;
+            names.push(item.name);
+        }
+
+        const result = `Bought furniture: ${names.join(' ')}
+            Total price: ${total.toFixed(2)}
+            Average decoration factor: ${decFactor / furniture.length}`;
+
+        output.value = result;
+    }
+
 }
--- DOMEvents/Locked-Profile/app.js
+++ DOMEvents/Locked-Profile/app.js
@@ -1,3 +1,30 @@
 function lockedProfile() {
-    console.log('TODO...')
+    // add toggle eventListener to all profile buttons
+
+    //find associated profile details
+    // if its visible -> hide it and display label 'Show more'
+    // otherwise -> show it and display label 'Hide it'
+
+    Array.from(document.querySelectorAll('.profile button')).forEach(btn =>
+        btn.addEventListener('click', onToggle));
+
+    function onToggle(e){
+        const profile = e.target.parentElement;
+        const isActive = profile.querySelector('input[type="radio"][value= "unlock"]').checked;
+        if (isActive){
+            const infoDif = profile.querySelector('div');
+            /*
+            const infoDif = Array
+                .from(e.target.parentElement.querySelectorAll('div'))
+                .find(d => d.id.includes('HiddenFields'));
+                */
+            if (e.target.textContent === 'Show more'){
+                infoDif.style.display = 'block';
+                e.target.textContent = 'Hide it';
+            }else {
+                infoDif.style.display = '';
+                e.target.textContent = 'Show more';
+            }
+        }
+    }
 }
